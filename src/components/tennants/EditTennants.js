a650eced8125b7c1ee1e9275e2aae6967d4ef8eb
pavan5745 <112757658+pavan5745@users.noreply.github.com>
2024-08-25 09:44:57 +0530

    Update EditTennants.js

--- src/components/tennants/EditTennants.js
+++ src/components/tennants/EditTennants.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/v1"; // Update with your actual API URL
+const API_URL = "https://my-hostel-api.onrender.com/api/v1"; // Update with your actual API URL
 const token = localStorage.getItem("jwt");
 const hostelId = "66adf998820d83f1797c0613";
 
